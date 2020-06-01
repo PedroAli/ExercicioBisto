4367c5fcc7ee9428a1554f63afd1b1ee2c1ad39d
pedro ali <pedroali78@yahoo.com>
2020-06-01 18:44:29 -0300

    Adicionar arquivos de projeto.

---
+++ Lista/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista
+{
+    public class Aluno
+    {
+        public Aluno() { }
+
+        public string Nome { get; set; }
+        public int Nota { get; set; }
+        public Aluno(int nota, string nome)
+        {
+            this.Nota = nota;
+            this.Nome = nome;
+        }
+    }
+}
---
+++ Lista/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static List<Aluno> listaDeAlunos;
+        static void Main(string[] args)
+        {
+            listaDeAlunos = new List<Aluno>();
+            listaDeAlunos.Add(new Aluno(5,"pedro"));
+            listaDeAlunos.Add(new Aluno(10, "João"));
+
+            foreach (var Aluno in listaDeAlunos)
+            {
+                Console.WriteLine(Aluno.Nota + " " + Aluno.Nome);
+            }
+
+        }
+    }
+}
